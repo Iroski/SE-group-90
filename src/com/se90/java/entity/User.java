9a5b1477471100deb98145b1ca6d54c0b2fa3c19
Iroski <50069574+Iroski@users.noreply.github.com>
2021-03-29 17:11:29 +0800

    添加部分jar，完成项目架构

---
+++ src/com/se90/java/entity/User.java
@@ -0,0 +1,16 @@
+package com.se90.java.entity;
+
+
+import lombok.Data;
+
+/**
+ * @author ：YanBo Zhang
+ * @date ：Created in 2021 2021/3/29 16:50
+ * @description：
+ * @modified By：
+ * @version:
+ */
+@Data
+public class User {
+
+}
