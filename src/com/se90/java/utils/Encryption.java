78a9a30944fe5fe827d9c3cfb5fa42362d13d79c
Iroski <50069574+Iroski@users.noreply.github.com>
2021-03-30 00:05:59 +0800

    更新 jar包和Dao层 MD5加密

--- src/com/se90/java/utils/Encryption.java
+++ src/com/se90/java/utils/Encryption.java
@@ -1,6 +1,10 @@
 package com.se90.java.utils;
 
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * @author ：YanBo Zhang
  * @date ：Created in 2021 2021/3/29 19:30
@@ -9,7 +13,22 @@ import java.security.MessageDigest;
  * @version:
  */
 public class Encryption {
-    public String EncoderByMd5(String str)  {
-        return null;
+    public static String getMD5Str(String str) {
+
+        byte[] digest = null;
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("md5");
+            digest  = md5.digest(str.getBytes("utf-8"));
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        //16 means converted to hexadecimal number
+        assert digest != null;
+        String md5Str = new BigInteger(1, digest).toString(16);
+        return md5Str;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMD5Str("112133"));
     }
 }
