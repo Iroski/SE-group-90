adcccb31577f622271d3b5735e18a90689b8d4bc
Iroski <50069574+Iroski@users.noreply.github.com>
2021-03-31 23:46:08 +0800

    ADD base主界面与base读写类

---
+++ src/com/se90/java/dao/EntityType.java
@@ -0,0 +1,11 @@
+package com.se90.java.dao;
+
+/**
+ * @author :YanBo Zhang
+ * @version :
+ * @date :Created in 2021 2021/3/31 22:47
+ * @description:
+ * @modified By:
+ */
+public enum EntityType {
+}
