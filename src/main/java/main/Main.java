ea3f6f7812bd47b2a3dfb74aa64cd320c34d2e21
SexyCrucian <2068606306@qq.com>
2021-04-05 17:59:21 +0800

    [F] Main函数删了没用的

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,61 +3,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
-
-    private Stage primaryStage;
-    public AnchorPane rootLayout;
-
-    public AnchorPane getRootLayout() {
-        return rootLayout;
-    }
-
-    public void setRootLayout(AnchorPane rootLayout) {
-        this.rootLayout = rootLayout;
-    }
-
-    /**
-     * Initializes the root layout.
-     */
-    public void initRootLayout() {
-        try {
-            // Load root layout from fxml file.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource("/view/fxml/BaseMainPage.fxml"));
-            rootLayout = (AnchorPane) loader.load();
-            // Show the scene containing the root layout.
-            Scene scene = new Scene(rootLayout);
-            primaryStage.setScene(scene);
-
-            primaryStage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Shows the person overview inside the root layout.
-     */
-    public void showPersonOverview() {
-        try {
-            // Load person overview.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource("/view/fxml/Coach.fxml"));
-            AnchorPane coaches = (AnchorPane) loader.load();
-            // Set person overview into the center of root layout.
-            rootLayout.getChildren().add( 2, coaches);
-            coaches.setLayoutX(200);
-            coaches.setLayoutY(75);
-            // Give the controller access to the main app.
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/view/fxml/LoginPage.fxml"));
