14fd1c76f0db04e096a10771b8a0ee5102d9364a
Yuhang-Lu <lu.yuhang2000@outlook.com>
2021-04-10 19:46:58 +0800

    video page, main page, profile page and account page

--- src/main/java/controller/BasePageController.java
+++ src/main/java/controller/BasePageController.java
@@ -1,8 +1,11 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
@@ -16,7 +19,6 @@ public class BasePageController {
     public Button userName;
     @FXML
     Button b_home;
-
     Image image = new Image("view/images/1.png");
 
     public void buttonColorChange1(MouseEvent me){
@@ -58,14 +60,14 @@ public class BasePageController {
         stage.setTitle("Videos");
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("/view/fxml/VideoPage.fxml"));
-        AnchorPane coaches = (AnchorPane) loader.load();
+        AnchorPane video = (AnchorPane) loader.load();
         // Set person overview into the center of root layout.
         AnchorPane anchorPane= (AnchorPane) stage.getScene().getRoot();
         anchorPane.getChildren().remove(2);
-        anchorPane.getChildren().add(2, coaches);
+        anchorPane.getChildren().add(2, video);
 
-        coaches.setLayoutX(200);
-        coaches.setLayoutY(75);
+        video.setLayoutX(200);
+        video.setLayoutY(75);
     }
     public void goToMyLesson(MouseEvent mouseEvent) throws IOException {
         //choosedCoach = (Coach) photo.getUserData();
@@ -90,13 +92,46 @@ public class BasePageController {
         stage.setTitle("Home");
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("/view/fxml/MainPage.fxml"));
-        AnchorPane coaches = (AnchorPane) loader.load();
+        AnchorPane home = (AnchorPane) loader.load();
         // Set person overview into the center of root layout.
         AnchorPane anchorPane= (AnchorPane) stage.getScene().getRoot();
         anchorPane.getChildren().remove(2);
-        anchorPane.getChildren().add(2, coaches);
+        anchorPane.getChildren().add(2, home);
 
-        coaches.setLayoutX(200);
-        coaches.setLayoutY(75);
+        home.setLayoutX(200);
+        home.setLayoutY(75);
     }
+    public void goToAccount(MouseEvent mouseEvent) throws IOException {
+        //choosedCoach = (Coach) photo.getUserData();
+
+        Stage stage = (Stage) b_home.getScene().getWindow();
+        stage.setTitle("Account");
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/view/fxml/Account.fxml"));
+        AnchorPane account = (AnchorPane) loader.load();
+        // Set person overview into the center of root layout.
+        AnchorPane anchorPane= (AnchorPane) stage.getScene().getRoot();
+        anchorPane.getChildren().remove(2);
+        anchorPane.getChildren().add(2, account);
+
+        account.setLayoutX(200);
+        account.setLayoutY(75);
+    }
+    public void goToProfile(MouseEvent mouseEvent) throws IOException {
+        //choosedCoach = (Coach) photo.getUserData();
+
+        Stage stage = (Stage) b_home.getScene().getWindow();
+        stage.setTitle("Profile");
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/view/fxml/Profile.fxml"));
+        AnchorPane profile = (AnchorPane) loader.load();
+        // Set person overview into the center of root layout.
+        AnchorPane anchorPane= (AnchorPane) stage.getScene().getRoot();
+        anchorPane.getChildren().remove(2);
+        anchorPane.getChildren().add(2, profile);
+
+        profile.setLayoutX(200);
+        profile.setLayoutY(75);
+    }
+
 }
