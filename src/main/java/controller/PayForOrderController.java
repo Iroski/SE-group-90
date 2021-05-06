b257baedb3e8988d6f8391bcc1cbfcfb557fb0a5
lzh0o0 <1476648056@qq.com>
2021-05-06 20:26:51 +0800

    [F] MOD: 完成余额够情况下订课

--- src/main/java/controller/PayForOrderController.java
+++ src/main/java/controller/PayForOrderController.java
@@ -1,4 +1,36 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class PayForOrderController {
+    private AtomicBoolean ifPay  = new AtomicBoolean(false);
+
+    @FXML
+    public Button pay;
+    @FXML
+    public  Button notPay;
+
+    @FXML
+    public void initialize() {}
+
+    public AtomicBoolean getIfPay(){
+        return this.ifPay;
+    }
+
+    private void setIfPay(){
+        this.ifPay.set(true);
+    }
+
+    public void handleChoose(MouseEvent mouseEvent){
+         Button button = (Button) mouseEvent.getSource();
+         if(button.equals(pay)){
+             setIfPay();
+         }
+         Stage stage = (Stage) pay.getScene().getWindow();
+         stage.close();
+    }
 }
