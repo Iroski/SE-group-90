707369c46e00d1d6e366e11e6b4479c578f98fbf
Iroski <50069574+Iroski@users.noreply.github.com>
2021-05-26 23:16:35 +0800

    wssb

--- src/main/java/main/Launcher.java
+++ src/main/java/main/Launcher.java
@@ -1,15 +1,8 @@
 package main;
 
 import javafx.application.Application;
-import model.dao.base.DataHouse;
 
 public class Launcher {
-    static DataHouse db;
-    public static void main(String[] args) {
-
-        db= DataHouse.getInstance();
-        db.init("src/main/resources/database");
-        Application.launch(Main.class, args);
-    }
+    public static void main(String args[]) {  Application.launch(Main.class,args);}
 }
 
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,11 +5,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import model.dao.base.DataHouse;
+import model.utils.ResourceLoader;
 
 import java.io.IOException;
 
 public class Main extends Application {
 
+    static DataHouse db;
     public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/view/fxml/LoginPage.fxml"));
         primaryStage.setTitle("Login");
@@ -17,6 +19,8 @@ public class Main extends Application {
         primaryStage.setResizable(false);
         primaryStage.show();
 
+        db= DataHouse.getInstance();
+        db.init("src/main/resources/database");
     }
 
 }
