76c4d4c6ad6b6bc9452fd0741d735e28d8e94c27
Iroski <50069574+Iroski@users.noreply.github.com>
2021-05-26 22:36:08 +0800

    [B] MOD:修改手机号验证逻辑

--- src/main/java/model/utils/CheckUserInfoValidation.java
+++ src/main/java/model/utils/CheckUserInfoValidation.java
@@ -15,7 +15,7 @@ public class CheckUserInfoValidation {
     }
 
     public static boolean checkMobile(String mobile) {
-        return Pattern.matches("((^(13|15|18)[0-9]{9}$)|(^0[1,2]{1}\\d{1}-?\\d{8}$)|(^0[3-9] {1}\\d{2}-?\\d{7,8}$)|(^0[1,2]{1}\\d{1}-?\\d{8}-(\\d{1,4})$)|(^0[3-9]{1}\\d{2}-? \\d{7,8}-(\\d{1,4})$))", mobile);
+        return isNumeric(mobile);
     }
 
     public static boolean isNumeric(String str){
