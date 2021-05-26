752aa3918a387de4932f0c6a3c93f13fe03028cf
Iroski <50069574+Iroski@users.noreply.github.com>
2021-05-26 22:06:11 +0800

    [B] ADD: MVN打包，规范代码

--- src/main/java/main/Launcher.java
+++ src/main/java/main/Launcher.java
@@ -8,12 +8,7 @@ public class Launcher {
     public static void main(String[] args) {
 
         db= DataHouse.getInstance();
-//        if(args==null||args.length==0){
-//            System.out.println("no args");
-//            db.init("database");
-//        }
-//        else
-            db.init("src/main/resources/database");
+        db.init("src/main/resources/database");
         Application.launch(Main.class, args);
     }
 }
