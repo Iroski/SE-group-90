4bc9035fe4c983f340d94a0fa531b2ca4923a301
SexyCrucian <2068606306@qq.com>
2021-05-27 12:38:11 +0800

    [F]MOD: 更改了pom里的路径

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,6 +5,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import model.dao.base.DataHouse;
+import model.service.AccountService;
+import model.service.LiveLessonService;
+import model.utils.ResourceLoader;
 
 import java.io.IOException;
 
@@ -18,8 +21,14 @@ public class Main extends Application {
         primaryStage.setResizable(false);
         primaryStage.show();
 
+//        db= DataHouse.getInstance();
+//        db.init("src/main/resources/database");
         db= DataHouse.getInstance();
         db.init("src/main/resources/database");
+        ResourceLoader.staticVideoLoader("src/main/resources/view/videos");
+        ResourceLoader.staticDefaultProfilePhotoLoader("src/main/resources/view/images/default/profilephoto");
+        new AccountService().createAccountForDeletedInfo();
+        new LiveLessonService().createInfoForDeleteInfo();
     }
 
 }
