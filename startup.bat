da16a38bd35fd74e4c30545649b8d3959bf6fed7
Zhang Yanbo <50069574+Iroski@users.noreply.github.com>
2021-05-28 10:46:08 +0800

    MOD:命令行启动
    * MOD:命令行启动
    
    * MOD:POM注释
    
    * fix error push

--- startup.bat
+++ startup.bat
@@ -4,6 +4,7 @@ xcopy /E lib\*.* out\lib\
 xcopy /E  src\main\resources\view\*.* out\view\
 xcopy /E  src\main\resources\video\*.* out\video\
 xcopy /E  src\main\resources\database\*.* out\src\main\resources\database\
+xcopy /E out\view\*.* out\src\main\resources\view\
 cd out
 java -cp lib\guava-20.0.jar;lib\annotations-20.1.0.jar;lib\fastjson-1.2.75.jar;lib\javafx-base-15.0.1-win.jar;lib\javafx-base-15.0.1.jar;lib\javafx-controls-15.0.1.jar;lib\javafx-controls-15.0.1-win.jar;lib\javafx-fxml-15.0.1-win.jar;lib\javafx-fxml-15.0.1.jar;lib\javafx-graphics-15.0.1.jar;lib\javafx-graphics-15.0.1-win.jar;lib\javafx-media-15.0.1.jar;lib\javafx-media-15.0.1-win.jar;lib\javafx-swing-15.0.1.jar;lib\javafx-swing-15.0.1-win.jar;lib\javafx-web-15.0.1.jar;lib\javafx-web-15.0.1-win.jar;lib\lombok-1.18.10.jar;lib\javassist-3.21.0-GA.jar;lib\reflections-0.9.11.jar;.  main.Launcher
 cd ..
